f7150425892fc2bba2d9e01a6c306e5fa4642cc8
xy <xy@luningdeMacBook.local>
2020-02-28 17:50:01 -0800

    fix conf file

--- lib/utils.go
+++ lib/utils.go
@@ -28,11 +28,16 @@ var (
 	urlConf        *gopsu.ConfData
 	linuxSSLCopy   = filepath.Join(gopsu.GetExecDir(), "sslcopy.sh")
 	windowsSSLCopy = filepath.Join(gopsu.GetExecDir(), "sslcopy.bat")
-	domainList     = []string{"*.shwlst.com"}
+	domainList     = []string{"*.shwlst.com,*.wlst.vip"}
 )
 
 // LoadExtConfigure 载入除标准配置外的自定义配置内容（可选）
 func LoadExtConfigure(f string) {
-	urlConf, _ = gopsu.LoadConfig(f)
-	domainList = strings.Split(urlConf.GetItemDefault("dnspod_list", "*.shwlst.com", "要管理的dnspod域名列表"), ",")
+	var err error
+	urlConf, err = gopsu.LoadConfig(f)
+	if err != nil {
+		println("Load configure file error:" + err.Error())
+	}
+	domainList = strings.Split(urlConf.GetItemDefault("dnspod_list", "*.shwlst.com,*.wlst.vip", "要管理的dnspod域名列表"), ",")
+	urlConf.Save()
 }
