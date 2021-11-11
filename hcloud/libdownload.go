de00a8214b27ba002e76b8fcceee245ff2b971a7
unknow <beunknow@outlook.com>
2021-11-11 09:21:15 +0800

    精简httphere

---
+++ hcloud/libdownload.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tidwall/sjson"
+	"github.com/xyzj/gopsu"
+)
+
+type videoinfo struct {
+	url    string
+	format string
+	try    int
+}
+
+var (
+	// chanHTTPDownloader    = make(chan *videoinfo, 100)
+	chanYoutubeDownloader = make(chan *videoinfo, 100)
+)
+
+func tdlb(c *gin.Context) {
+	switch c.Request.Method {
+	case "GET":
+		c.Header("Content-Type", "text/html")
+		c.String(200, tplth)
+	case "POST":
+		vlist := strings.Split(c.Param("vlist"), "\n")
+		for _, vl := range vlist {
+			if gopsu.TrimString(vl) == "" {
+				continue
+			}
+		START:
+			s := strings.Split(vl, ":")
+			switch s[0] {
+			case "thunder":
+				ss, _ := base64.StdEncoding.DecodeString(s[1][2:])
+				a, _ := gopsu.GbkToUtf8(ss[2 : len(ss)-2])
+				vl = string(a)
+				goto START
+			case "http", "https":
+				if strings.Contains(vl, "www.youtube.com") {
+					vl = strings.ReplaceAll(vl, "&pp=sAQA", "")
+					if strings.Contains(vl, "&&") {
+						x := strings.Split(gopsu.TrimString(vl), "&&")
+						chanYoutubeDownloader <- &videoinfo{url: x[0], format: x[1]}
+					} else {
+						chanYoutubeDownloader <- &videoinfo{url: vl, format: ""}
+					}
+				} else {
+					furl := vl
+					if !strings.Contains(vl, "%") {
+						idx := strings.LastIndex(vl, "/")
+						furl = vl[:idx+1] + url.QueryEscape(vl[idx+1:])
+					}
+					rpcToAria2(furl)
+				}
+			case "magnet":
+				rpcToAria2(vl)
+			}
+			// magnet:?xt=urn:btih:6f2359c12381e22c2fc0ea0b86fb9754c0ca999d
+		}
+		c.String(200, "These links have been added to the download queue...")
+	}
+}
+
+func rpcToAria2(vl string) {
+	s, _ := sjson.SetBytes([]byte{}, "jsonrpc", "2.0")
+	s, _ = sjson.SetBytes(s, "id", fmt.Sprintf("%d", time.Now().UnixNano()))
+	s, _ = sjson.SetBytes(s, "method", "aria2.addUri")
+	s, _ = sjson.SetBytes(s, "params.0.0", vl)
+	ss := strings.ReplaceAll(base64.URLEncoding.EncodeToString(s), "=", "%3D")
+	req, _ := http.NewRequest("GET", *aria2+"/jsonrpc?params="+ss, strings.NewReader(""))
+	resp, err := httpClientPool.Do(req)
+	shellName := "/tmp/" + gopsu.CalcCRC32String([]byte(vl)) + ".aria2.log"
+	if err != nil {
+		ioutil.WriteFile(shellName, []byte(vl+"\n\n"+err.Error()), 0664)
+	} else {
+		body, _ := ioutil.ReadAll(resp.Body)
+		if strings.Contains(string(body), "error") {
+			ioutil.WriteFile(shellName, []byte(vl+"\n\n"+string(body)), 0664)
+		}
+	}
+}
+
+func youtubeControl() {
+	videoNameReplacer := strings.NewReplacer("WARNING:Unabletodownloadwebpage:Nostatuslinereceived-theserverhasclosedtheconnection", "",
+		"、", ";", "%", "", "\n", "", "\r", "", "；", ";", "：", ":", "（", "", "）", "", "？", "", " ", "", "《", "<", "》", ">", "！", "", "，", ",", "。", "")
+RUN:
+	func() {
+		defer func() {
+			if err := recover(); err != nil {
+				println(err.(error).Error())
+			}
+		}()
+		var scmd bytes.Buffer
+		var cmd *exec.Cmd
+		var shellName string
+		var videoName = "%(title)s"
+		for vi := range chanYoutubeDownloader {
+			videoName = "%(title)s"
+			if gopsu.TrimString(vi.url) == "" || vi.try >= 5 {
+				continue
+			}
+			shellName = "/tmp/" + gopsu.CalcCRC32String([]byte(vi.url)) + "-name.sh"
+			scmd.Reset()
+			scmd.WriteString("#!/bin/bash\n\n")
+			scmd.WriteString("youtube-dl ")
+			scmd.WriteString("--proxy='http://127.0.0.1:8119' ")
+			scmd.WriteString("--get-filename ")
+			scmd.WriteString("-o '%(title)s' ")
+			scmd.WriteString("'" + vi.url + "'")
+			scmd.WriteString(" && \\\nrm $0\n")
+			ioutil.WriteFile(shellName, scmd.Bytes(), 0755)
+			cmd = exec.Command(shellName)
+			if b, err := cmd.CombinedOutput(); err == nil && !strings.Contains(string(b), "urlopenerror") {
+				videoName = videoNameReplacer.Replace(string(b))
+				if len(videoName) > 230 {
+					videoName = videoName[:230]
+				}
+			}
+			shellName = "/tmp/" + gopsu.CalcCRC32String([]byte(vi.url)) + ".sh"
+			if isExist(shellName) && vi.format == "" {
+				goto DOWN
+			}
+			scmd.Reset()
+			scmd.WriteString("#!/bin/bash\n\n")
+
+			scmd.WriteString("youtube-dl ")
+			scmd.WriteString("--proxy='http://127.0.0.1:8119' ")
+			scmd.WriteString("--continue ")
+			scmd.WriteString("--write-thumbnail ")
+			scmd.WriteString("--write-sub --write-auto-sub --sub-lang 'en,en-US,zh-Hant' ")
+			// scmd.WriteString("--mark-watched ")
+			// scmd.WriteString("--youtube-skip-dash-manifest ")
+			scmd.WriteString("--skip-unavailable-fragments ")
+			// scmd.WriteString("--abort-on-unavailable-fragment ")
+			scmd.WriteString("--no-mtime ")
+			scmd.WriteString("--buffer-size 256k ")
+			// scmd.WriteString("--recode-video mp4 ")
+			scmd.WriteString("-o '" + *ydir + videoName + ".%(ext)s' ")
+			if vi.format == "" {
+				vi.format = "133+140/242+250/242+251/133+250/133+251/18"
+			}
+			scmd.WriteString("-f '" + vi.format + "' ")
+			if strings.HasPrefix(vi.url, "http") {
+				scmd.WriteString("'" + vi.url + "'")
+			} else {
+				scmd.WriteString("-- " + vi.url)
+			}
+			scmd.WriteString(" && \\\nrm $0\n")
+			ioutil.WriteFile(shellName, scmd.Bytes(), 0755)
+		DOWN:
+			time.Sleep(time.Second * time.Duration(rand.Int31n(5)+10))
+			cmd = exec.Command(shellName)
+			b, err := cmd.CombinedOutput()
+			if err != nil {
+				b = append(b, []byte("\n"+err.Error()+"\n")...)
+				ioutil.WriteFile(shellName+".log", b, 0664)
+			}
+			time.Sleep(time.Second * time.Duration(rand.Int31n(5)+3))
+			if isExist(shellName) {
+				if !strings.Contains(strings.ToLower(string(b)), "error") {
+					vi.try++
+				}
+				chanYoutubeDownloader <- vi
+			} else {
+				os.Remove(shellName + ".log")
+			}
+		}
+	}()
+	time.Sleep(time.Second)
+	goto RUN
+}
---
+++ hcloud/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"unsafe"
+
+	ginmiddleware "github.com/xyzj/gopsu/gin-middleware"
+)
+
+var (
+	version     = "0.0.0"
+	goVersion   = ""
+	buildDate   = ""
+	platform    = ""
+	author      = "Xu Yuan"              // 你的名字
+	programName = "golang micro service" // 服务名称
+)
+
+type sliceFlag []string
+
+func (f *sliceFlag) String() string {
+	return fmt.Sprintf("%v", []string(*f))
+}
+
+func (f *sliceFlag) Set(value string) error {
+	*f = append(*f, value)
+	return nil
+}
+
+var (
+	port  = flag.Int("http", 8019, "set http port")
+	ports = flag.Int("https", 0, "set https port")
+	cert  = flag.String("cert", "", "cert file path")
+	key   = flag.String("key", "", "key file path")
+	auth  = flag.Bool("auth", false, "enable basicauth")
+	debug = flag.Bool("debug", false, "run in debug mode")
+	html  = flag.String("html", "", "something like nginx")
+	ydir  = flag.String("ydir", "", "set youtube download dir")
+	aria2 = flag.String("aria2", "", "set aria2 json rpc url")
+	// 帮助信息
+	help = flag.Bool("help", false, "print help message and exit.")
+	dirs sliceFlag
+	wtv  sliceFlag
+)
+
+func unsafeString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func isExist(p string) bool {
+	if p == "" {
+		return false
+	}
+	_, err := os.Stat(p)
+	return err == nil || os.IsExist(err)
+}
+
+func main() {
+	// http 静态目录
+	flag.Var(&dirs, "dir", "example: -dir=name:path -dir name2:path2")
+	// web gallery 目录
+	flag.Var(&wtv, "wtv", "example: -wtv=name:path -wtv name2:path2")
+	flag.Parse()
+	// 参数处理
+	if *aria2 == "" {
+		*aria2 = "http://192.168.1.99:2052"
+	}
+	if *ydir == "" {
+		*ydir = "/home/xy/mm/tv/news/"
+	}
+	if !strings.HasSuffix(*ydir, "/") {
+		*ydir += "/"
+	}
+	// 启动youtube下载控制
+	for i := 0; i < 3; i++ {
+		go youtubeControl()
+	}
+
+	opt := &ginmiddleware.ServiceOption{
+		HTTPPort:   *port,
+		HTTPSPort:  *ports,
+		CertFile:   *cert,
+		KeyFile:    *key,
+		Debug:      *debug,
+		EngineFunc: routeEngine,
+	}
+	ginmiddleware.ListenAndServeWithOption(opt)
+}
