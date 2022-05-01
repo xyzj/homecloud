16c2d731d0a34a782ff70c49ab5d37451882e780
unknow <beunknow@outlook.com>
2022-05-01 18:54:13 +0800

    add vpstools and dockerfile

--- vpstools/libhttp.go
+++ vpstools/libhttp.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"crypto/tls"
-	"fmt"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -54,40 +52,18 @@ func routeEngine() *gin.Engine {
 		AllowMethods:     []string{"*"},
 		AllowHeaders:     []string{"*"},
 	}))
-	// 渲染模板
-	r.HTMLRender = multiRender()
-
-	if *html != "" {
-		r.Static("/html", *html)
-		r.GET("/index", func(c *gin.Context) {
-			c.Redirect(http.StatusTemporaryRedirect, "/html/index.html")
-		})
-	}
 	// 主页
 	r.GET("/", remoteIP)
-	// 静态资源
-	r.StaticFS("/emb", http.FS(stat))
-
 	// 工具路由
 	// vps信息
 	r.GET("/tools/vpsinfo", vps4info)
-	// ariang
-	r.GET("/tools/ariang", func(c *gin.Context) {
-		c.Header("Content-Type", "text/html")
-		c.String(200, pageAriang)
-	})
-	// 上传
-	r.GET("/xyonly/upload", gin.BasicAuth(gin.Accounts{"whoareyou": "minamoto"}), fileUploadWeb)
-	r.POST("/xyonly/upload", fileUpload)
+
 	// md5编码
 	r.GET("/tools/md5", md5String)
 	r.POST("/tools/md5", ginmiddleware.ReadParams(), md5String)
 	// 自定义编码
 	r.GET("/tools/coder", codeString)
 	r.POST("/tools/coder", ginmiddleware.ReadParams(), codeString)
-	// 资源下载，youtubedl，aria2
-	r.GET("/tools/dl", tdlb)
-	r.POST("/tools/dl", ginmiddleware.ReadParams(), tdlb)
 	// 查看缓存ip
 	r.GET("/tools/cachedip", func(c *gin.Context) { c.String(200, ipCached) })
 	// 向cf更新home的最新ip
@@ -100,32 +76,6 @@ func routeEngine() *gin.Engine {
 	gcert.GET("/namesilo/:do", certNamesilo)
 	gcert.GET("/dnspod/:do", certDNSPod)
 	gcert.GET("/cloudflare/:do", certCloudflare)
-
-	// 静态资源路由
-	println("----- enable routes: -----")
-	for k, v := range dirs {
-		if strings.Contains(v, ":") {
-			r.StaticFS("/"+strings.Split(v, ":")[0], http.Dir(strings.Split(v, ":")[1]))
-			println(fmt.Sprintf("dir %d. /%s/", k+1, strings.Split(v, ":")[0]))
-		}
-	}
-
-	// webtv 路由
-	var gv *gin.RouterGroup
-	if *auth {
-		gv = r.Group("/v", gin.BasicAuth(gin.Accounts{
-			"golang":     "based",
-			"thewhyofgo": "simple&fast",
-		}), ginmiddleware.ReadParams())
-	} else {
-		gv = r.Group("/v", ginmiddleware.ReadParams())
-	}
-	for _, v := range wtv {
-		if strings.Contains(v, ":") {
-			gv.GET("/"+strings.Split(v, ":")[0], runVideojs(strings.Split(v, ":")[0], strings.Split(v, ":")[1]))
-			r.StaticFS("/v-"+strings.Split(v, ":")[0], http.Dir(strings.Split(v, ":")[1]))
-		}
-	}
 
 	return r
 }
---
+++ vpstools/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+	"unsafe"
+
+	"github.com/xyzj/gopsu"
+
+	ginmiddleware "github.com/xyzj/gopsu/gin-middleware"
+)
+
+var (
+	version     = "0.0.0"
+	goVersion   = ""
+	buildDate   = ""
+	platform    = ""
+	author      = "Xu Yuan"              // 你的名字
+	programName = "golang micro service" // 服务名称
+)
+
+type sliceFlag []string
+
+func (f *sliceFlag) String() string {
+	return fmt.Sprintf("%v", []string(*f))
+}
+
+func (f *sliceFlag) Set(value string) error {
+	*f = append(*f, value)
+	return nil
+}
+
+var (
+	port  = flag.Int("http", 2082, "set http port")
+	ports = flag.Int("https", 0, "set https port")
+	cert  = flag.String("cert", "", "cert file path")
+	key   = flag.String("key", "", "key file path")
+	auth  = flag.Bool("auth", false, "enable basicauth")
+	debug = flag.Bool("debug", false, "run in debug mode")
+	// 帮助信息
+	help = flag.Bool("help", false, "print help message and exit")
+)
+
+func unsafeString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func isExist(p string) bool {
+	if p == "" {
+		return false
+	}
+	_, err := os.Stat(p)
+	return err == nil || os.IsExist(err)
+}
+
+func main() {
+	flag.Parse()
+	if *help {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+	go func() {
+		ioutil.WriteFile(gopsu.JoinPathFromHere("cfrenew.log"), []byte(certCloudflareTools("renew")), 0664)
+		time.Sleep(time.Hour * 240)
+	}()
+	opt := &ginmiddleware.ServiceOption{
+		HTTPPort:   *port,
+		HTTPSPort:  *ports,
+		CertFile:   *cert,
+		KeyFile:    *key,
+		Debug:      *debug,
+		EngineFunc: routeEngine,
+	}
+	ginmiddleware.ListenAndServeWithOption(opt)
+}
