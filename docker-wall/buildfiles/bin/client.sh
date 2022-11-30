b2efbda012de081129a85ca805971332e1e9ad89
unknow <beunknow@outlook.com>
2022-11-30 15:24:22 +0800

    upgrade docker-wall

--- docker-wall/buildfiles/bin/client.sh
+++ docker-wall/buildfiles/bin/client.sh
@@ -4,4 +4,8 @@ start-stop-daemon --stop -p /run/v2client.pid
 
 sleep 1
 
-start-stop-daemon --start --background -m -p /run/v2client.pid /usr/bin/v2ray -- -c /root/bin/v2client.json
+start-stop-daemon --start --background -m -p /run/v2client.pid /root/bin/v2ray -- run -c /root/bin/v2less.json
+
+sleep 1
+
+start-stop-daemon --start --background -m -p /run/caddy.pid --exec /usr/sbin/caddy -- run --config /etc/caddy/Caddyfile
--- docker-wall/buildfiles/bin/server.sh
+++ docker-wall/buildfiles/bin/server.sh
@@ -5,7 +5,7 @@ start-stop-daemon -p /run/v2ray.pid --stop
 
 sleep 1
 
-start-stop-daemon -m -p /run/v2ray.pid --start --background --exec /usr/bin/v2ray -- -c=/root/bin/v2server.json
+start-stop-daemon -m -p /run/v2ray.pid -d /root/bin --start --background --exec /root/bin/v2ray -- run -c /root/bin/v2server.json
 start-stop-daemon -m -p /run/vpstools.pid --start --background --exec /root/bin/vpstools -- -http=2052
 
 sleep 1
