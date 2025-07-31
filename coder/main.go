7f82f5882563ecb9ce7a7710e11550a50e335e89
unknow <beunknow@outlook.com>
2025-07-31 20:59:47 +0800

    可自动启动aria2c(需ssdctl)

--- coder/main.go
+++ coder/main.go
@@ -3,21 +3,42 @@ package main
 import (
 	"flag"
 
-	"github.com/xyzj/gopsu"
+	"github.com/xyzj/toolbox"
+	"github.com/xyzj/toolbox/crypto"
 )
 
 func main() {
-	encode := flag.String("e", "", "input the string you want to encode")
-	decode := flag.String("d", "", "input the string you want to decode")
+	old := flag.Bool("o", false, "use old version")
+	encode := flag.Bool("e", true, "Encode the input")
+	decode := flag.Bool("d", false, "Decode the input")
 	md := flag.String("md5", "", "count md5")
 	flag.Parse()
-	if *encode != "" {
-		println(gopsu.CodeString(*encode))
-	}
-	if *decode != "" {
-		println(gopsu.DecodeString(*decode))
-	}
 	if *md != "" {
-		println(gopsu.GetMD5(*md))
+		println(crypto.GetMD5(*md))
+		return
+	}
+	if *old {
+		if *decode || !*encode {
+			for _, v := range flag.Args() {
+				println(toolbox.DecodeString(v))
+			}
+			return
+		} else {
+			for _, v := range flag.Args() {
+				println(toolbox.CodeString(v))
+			}
+			return
+		}
+	}
+	if *decode {
+		for _, v := range flag.Args() {
+			println(crypto.DeobfuscationString(v))
+		}
+		return
+	} else {
+		for _, v := range flag.Args() {
+			println(crypto.ObfuscationString(v))
+		}
+		return
 	}
 }
